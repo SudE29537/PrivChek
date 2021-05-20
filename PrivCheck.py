8b2e59f10e664bea37451cf467581d6999b7dce5
SudE29537 <sude@sude.com.br>
2021-05-20 15:08:59 -0300

    case-insensitive

--- PrivCheck.py
+++ PrivCheck.py
@@ -1,11 +1,9 @@
 w = "wordlist.txt"
 d = "demofile.txt"
-
-keys = [key for key in (line.strip() for line in open(w)) if key]
-
+keys = [key for key in (line.strip().lower() for line in open(w)) if key]
 with open(d) as f:
     for line in f:
         for key in keys:
-            if key in line:
+            if key in line.lower():
                 print(line, end='')
                 break
